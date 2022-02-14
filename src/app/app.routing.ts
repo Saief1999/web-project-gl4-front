639e1f1ac64a887c91e24a8ebe6b77f48fad3a7b
Ramzi-dev611 <ramzi.latrous@insat.u-carthage.tn>
2022-02-14 10:19:32 +0100

    add guards

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -15,9 +15,11 @@ import { CinemasPageComponent } from './pages/cinemas/cinemas-page.component';
 import { NotFoundPageComponent } from './not-found/not-found-page.component';
 import { ConfirmEmailComponent } from './pages/auth/confirm-email/confirm-email.component';
 import { AccountPageComponent } from './pages/account/account-page.component';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { UnauthenticatedGuard } from './guards/unauthenticated.guard';
+// import { AuthenticatedGuad } from './guards/authenticated.guard';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'confirm-email', component: ConfirmEmailComponent },
   { path: 'home', component: ComponentsComponent },
   { path: 'user-profile', component: ProfileComponent },
   { path: 'signup', component: SignupComponent },
@@ -26,13 +28,15 @@ const routes: Routes = [
   {
     path: 'register', component: RegistrationPageComponent,
     children: [
-      { path: '', redirectTo: "/register/login", pathMatch: 'full' },
-      { path: 'login', component: LoginCardComponent },
-      { path: 'signup', component: SignupCardComponent }
-    ]
+      { path: '', redirectTo: "/register/login", pathMatch: 'full', canActivate: [UnauthenticatedGuard] },
+      { path: 'login', component: LoginCardComponent, canActivate: [UnauthenticatedGuard] },
+      { path: 'signup', component: SignupCardComponent, canActivate: [UnauthenticatedGuard] }
+    ], 
+    canActivate: [UnauthenticatedGuard]
   },
-  { path: 'account', component: AccountPageComponent },
-  { path: 'cinemas', component: CinemasPageComponent },
+  { path: 'confirm-email', component: ConfirmEmailComponent, canActivate:[UserRoleGuard] },
+  { path: 'account', component: AccountPageComponent, canActivate: [UserRoleGuard] },
+  { path: 'cinemas', component: CinemasPageComponent, /*canActivate: [AuthenticatedGuard] */ },
   { path: '**', redirectTo: 'not-found' },
   { path: 'not-found', component: NotFoundPageComponent }
 ];
