19d751b0f1ed6caa0dbf5476da2972350749c138
saief1999 <saief_zaneti@yahoo.com>
2022-02-16 14:37:07 +0100

    added buttons for cinema

--- src/app/dto/cinemas/cinema.ts
+++ src/app/dto/cinemas/cinema.ts
@@ -13,5 +13,16 @@ export class Cinema {
     closingTime: string;
     
     imageUrl: string;
+
+    constructor() {
+        this._id = "";
+        this.name = "";
+        this.description = "";
+        this.address = "";
+        this.phone = 12345678;
+        this.openingTime = "";
+        this.closingTime = "";
+        this.imageUrl = "";
+    }
     
 }
